44cff4c24ca694498d6965450e92c5015494a8a0
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-19 00:08:54 -0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({origin:"http://localhost:5173", credentials:true}));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <Button>LMS</Button>
+  )
+}
+
+export default App
