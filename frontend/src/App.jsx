d134ca56b99ce6760afe95d444be4dfa06ac736b
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-19 11:50:09 -0500

    HeroSection Ui created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
+import HeroSection from "./pages/student/HeroSection";
+import MainLayout from "./layout/MainLayout";
 
 const App = () => {
   return (
     <main>
-      <Navbar />
+      
       <Routes>
-        <Route path="/" element={<Login />} />
+         {/* Home page */}
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HeroSection />} />
+        </Route>
+         {/* Login page */}
+        <Route path="/login" element={<Login />} />
       </Routes>
     </main>
   );
---
+++ frontend/src/pages/student/Courses.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Courses = () => {
+  return (
+    <div className='bg-gray-50'>
+        <div className='max-w-7xl mx-auto p-6'>
+            <h1 className='font-bold text-3xl text-center mb-10'>Our Courses</h1>
+
+        </div>
+    </div>
+  )
+}
+
+export default Courses
