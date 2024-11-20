f8ed534d0c6538454927d1e469752930ccfa5b0d
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-20 16:48:45 -0500

    Leacture model,controller and routes added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/db.js';
-import userRoutes from "./routes/userRoute.js"
-import courseRoutes from "./routes/courseRoute.js"
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoute.js";
+import courseRoutes from "./routes/courseRoute.js";
+import mediaRoutes from "./routes/mediaRoute.js";
 
 // Load environment variables
 dotenv.config();
@@ -14,18 +14,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //Db connection
-connectDB()
+connectDB();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin:"http://localhost:5173", credentials:true}));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 
 // Routes
-app.use("/api/v1/user", userRoutes)
-app.use("/api/v1/course", courseRoutes)
-
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/media", mediaRoutes);
 
 // Start the server
 app.listen(port, () => {
--- backend/routes/courseRoute.js
+++ backend/routes/courseRoute.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createCourse, editCourse, getCourseById, getCreatorCourses } from "../controllers/courseController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 import upload from "../utils/multer.js"
-import { createLecture, getCourseLecture } from "../controllers/lectureController.js";
+import { createLecture, getCourseLecture, getLectureById, removeLecture, updateLecture } from "../controllers/lectureController.js";
 
 
 const router = express.Router();
@@ -16,5 +16,8 @@ router.route("/:courseId").get(isAuthenticated,getCourseById);
 // For Lectures
 router.route("/:courseId/lecture").post(isAuthenticated, createLecture);
 router.route("/:courseId/lecture").get(isAuthenticated, getCourseLecture);
+router.route("/:courseId/lecture/:lectureId").put(isAuthenticated,updateLecture);
+router.route("/lecture/:lectureId").delete(isAuthenticated, removeLecture);
+router.route("/lecture/:lectureId").get(isAuthenticated, getLectureById);
 
 export default router;
