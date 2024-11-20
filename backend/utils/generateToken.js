52675519e1d9a530d62f8382af0bd284b606c367
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-19 19:19:09 -0500

    Minor change

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,14 +2,14 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (res, user, message) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
+    expiresIn: "30d",
   });
 
   res
     .cookie("token", token, {
       httpOnly: true,
       sameSite: "strict",
-      maxAge: 24 * 60 * 60 * 1000, // 1 day
+      maxAge: 30* 24 * 60 * 60 * 1000, 
     })
     .json({
       success: true,
