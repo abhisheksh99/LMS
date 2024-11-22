bf36b5fb6c3d27e4263ad660b3c36b1daf3ebc53
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-22 10:05:11 -0500

    CoureProgress model,controller and routes added

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/userRoute.js";
 import courseRoutes from "./routes/courseRoute.js";
 import mediaRoutes from "./routes/mediaRoute.js";
 import coursePurchaseRoutes from "./routes/coursePurchaseRoute.js"
+import courseProgressRoutes from "./routes/courseProgressRoute.js";
 
 // Load environment variables
 dotenv.config();
@@ -32,6 +33,7 @@ app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/media", mediaRoutes);
 app.use("/api/v1/purchase", coursePurchaseRoutes);
+app.use("/api/v1/progress", courseProgressRoutes);
 
 // Start the server
 app.listen(port, () => {
